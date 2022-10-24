440a504078b0dafb0df601824e9670a4fa26ec5d
Oleksii Akishev <akishev.oleksii@gmail.com>
2022-10-24 10:35:52 +0300

    АшÑFinal changes

--- src/main/java/akishev/Main.java
+++ src/main/java/akishev/Main.java
@@ -15,21 +15,16 @@ public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context
                 = new AnnotationConfigApplicationContext(AppConfig.class);
-
         PathHandler pathHandler = context.getBean(PathHandler.class);
         String inputPathFile = pathHandler.setInputFilePath();
-
         FileReader fileReader = context.getBean(FileReader.class);
         List<String> data = fileReader.readFromFile(inputPathFile);
         CarMapper carMapper = context.getBean(CarMapper.class);
-
         List<Car> cars = data.stream()
                 .map(carMapper::toModel)
                 .collect(Collectors.toList());
-
         CarService carService = context.getBean(CarService.class);
         cars.forEach(carService::add);
-
         AppInterface appInterface = context.getBean(AppInterface.class);
         appInterface.runApp();
     }
